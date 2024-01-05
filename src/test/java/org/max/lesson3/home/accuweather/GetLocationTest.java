fabd6389018142f246a325e1b1a7ca84d2cd76c7
«karina» <vlasova_karina1985@mail.ru>
2024-01-05 19:03:58 +0300

    Initial commit

---
+++ src/test/java/org/max/lesson3/home/accuweather/GetLocationTest.java
@@ -0,0 +1,108 @@
+package org.max.lesson3.home.accuweather;
+
+import io.restassured.http.ContentType;
+import io.restassured.http.Cookie;
+import io.restassured.http.Method;
+import io.restassured.path.json.JsonPath;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.max.lesson3.home.accuweather.location.Location;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.lessThan;
+
+
+public class GetLocationTest extends AccuweatherAbstractTest {
+
+    @Test
+    void getLocation_search_returnMinsk() {
+
+        List<Location> response = given()
+                .queryParam("apikey", getApiKey())
+                .queryParam("q", "Minsk")
+                .when()
+                .get(getBaseUrl() + "/locations/v1/cities/search")
+                .then()
+                .statusCode(200)
+                .time(Matchers.lessThan(2000L))
+                .extract()
+                .body().jsonPath().getList(".", Location.class);
+
+        Assertions.assertEquals(24, response.size());
+        Assertions.assertEquals("Minsk", response.get(0).getEnglishName());
+
+
+    }
+    @Test
+    void getVerifyingResponseData(){
+
+        JsonPath response = given()
+                .when()
+                .request(Method.GET,getBaseUrl()+"/locations/{version}/regions?" +
+                        "apikey={apiKey}", "v1", getApiKey())
+                .body()
+                .jsonPath();
+
+        assertThat(response.get("[0].ID"), equalTo("AFR"));
+        assertThat(response.get("[1].ID"), equalTo("ANT"));
+        assertThat(response.get("[2].ID"), equalTo("ARC"));
+
+
+        given().response().expect()
+                .body("[0].LocalizedName", equalTo("Africa"))
+                .body("[0].EnglishName", equalTo("Africa"))
+                .when()
+                .request(Method.GET,getBaseUrl()+"/locations/{version}/regions?" +
+                        "apikey={apiKey}", "v1", getApiKey())
+                .then()
+                .assertThat()
+                //.cookie("cookieName", "cookieValue")
+                .statusCode(200)
+                .statusLine("HTTP/1.1 200 OK")
+                .header("Connection", "keep-alive")
+                .contentType(ContentType.JSON)
+                .time(lessThan(2000L));
+
+    }
+    @Test
+    void getSpecifyingRequestDataTest() {
+        given()
+                .when()
+                .request(Method.GET,getBaseUrl()+"/locations/{version}/regions?" +
+                        "apikey={apiKey}", "v1", getApiKey())
+                .then()
+                .statusCode(200);
+
+        given()
+                .queryParam("apikey", getApiKey())
+                .pathParam("version", "v1")
+                .when()
+                .request(Method.GET,getBaseUrl()+"/locations/{version}/regions")
+                .then()
+                .statusCode(200);
+
+        given().cookie("username","max")
+                .cookie( new Cookie
+                        .Builder("some_cookie", "some_value")
+                        .setSecured(true)
+                        .setComment("some comment")
+                        .build())
+                .when()
+                .get(getBaseUrl()+"/locations/v1/regions?" +
+                        "apikey=" +getApiKey())
+                .then()
+                .statusCode(200);
+
+        given().headers("username","max")
+                .when()
+                .get(getBaseUrl()+"/locations/v1/regions?" +
+                        "apikey=" +getApiKey())
+                .then()
+                .statusCode(200);
+    }
+}
---
+++ src/test/java/org/max/lesson3/home/accuweather/GetWeatherOneDayTest.java
@@ -0,0 +1,36 @@
+package org.max.lesson3.home.accuweather;
+
+import io.restassured.http.Cookie;
+import io.restassured.http.Method;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.max.lesson3.seminar.accuweather.AccuweatherAbstractTest;
+import org.max.lesson3.seminar.accuweather.weather.Weather;
+import org.max.lesson3.seminar.spoonacular.ConvertAmountsDto;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.max.lesson3.home.accuweather.AccuweatherAbstractTest.getApiKey;
+import static org.max.lesson3.home.accuweather.AccuweatherAbstractTest.getBaseUrl;
+
+public class GetWeatherOneDayTest extends AccuweatherAbstractTest {
+
+    @Test
+    void getWeatherOneDay_shouldReturn() {
+
+        Weather response = given()
+                .queryParam("apikey", getApiKey())
+                .when()
+                .get(getBaseUrl()+"/forecasts/v1/daily/1day/294021")
+                .then()
+                .statusCode(200)
+                .time(Matchers.lessThan(2000l))
+                .extract()
+                .response()
+                .body().as(Weather.class);
+
+        Assertions.assertEquals(1,response.getDailyForecasts().size());
+    }
+}
